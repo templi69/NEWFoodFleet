234737b0e89d1848d474469ff3535a84e0804a93
templi69 <f8.taha.amer@gmail.com>
2026-01-19 21:58:11 +0500

    ghg

--- app/src/main/java/com/example/new_foodfleet/UserDashboard.java
+++ app/src/main/java/com/example/new_foodfleet/UserDashboard.java
@@ -50,7 +50,7 @@ public class UserDashboard extends AppCompatActivity {
 
         // Firebase reference
         restaurantRef = FirebaseDatabase.getInstance()
-                .getReference("restaurant");
+                .getReference("Restaurants");
 
         // Fetch restaurants from Firebase
         restaurantRef.addValueEventListener(new ValueEventListener() {
